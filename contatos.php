6f077fb0a1f2500806b31af6673cafa61ec32276
CarlosEmannoel16 <53618695+CarlosEmannoel16@users.noreply.github.com>
2019-12-10 21:09:48 -0300

    Create contatos.php

---
+++ contatos.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--icon de inicio-->
+    <link rel="icon" href="img/edivam motos.png">
+     <!--VERSÃO 2.0 -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.css">
+    <!--CSS DE ESTILO-->
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/estilo.css">
+    <!--titulo inicial-->
+    <title>Edivan Motos</title>
+  </head>
+  <body>
+    <div id="pagtotal">
+    <!--MENU INICIAL-->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">
+              <img src="img/edivam motos.png" alt="eidvan motos">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar" aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="textoNavbar">
+            <ul class="navbar-nav mr-auto"> 
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Serviços</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contato</a>
+              </li>
+            </ul>
+            <span class="navbar-text">
+             
+            </span>
+          </div>
+        </nav>
+
+        <!--FORMULARIO-->
+        <form class="formulario">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Endereço de email</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
+                  <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Nome Completo</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="EX: Alesson Sousa Viana">
+                </div>
+                <div class="form-group form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Deixar Salvo</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+              </form>
+              
+        
+            </div>
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="bootstrap-4.1.3-dist/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="bootstrap-4.1.3-dist/js/popper.min.js"></script>
+    <script src="bootstrap-4.1.3-dist/js/bootstrap.js"></script>
+  </body>
+</html>
