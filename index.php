d94ad24b774407e8572f813823465ee2979a14bd
CarlosEmannoel16 <53618695+CarlosEmannoel16@users.noreply.github.com>
2019-12-10 21:09:55 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--icon de inicio-->
+    <link rel="icon" href="img/edivam motos.png">
+     <!--VERSÃO 2.0 -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/home.css">
+    <!--CSS DE ESTILO-->
+    <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/estilo.css">
+    <script src="https://kit.fontawesome.com/13bff46990.js" crossorigin="anonymous"></script>
+    <!--titulo inicial-->
+    <title>Edivan Motos</title>
+  </head>
+  <body>
+    <!--MENU -->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark menu">
+          <a class="navbar-brand" href="#">
+              <i class="fas fa-cogs"></i>
+
+
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar" aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="textoNavbar">
+            <ul class="navbar-nav mr-auto"> 
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Serviços</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contato</a>
+              </li>
+            </ul>
+            <span class="navbar-text">
+             
+            </span>
+          </div>
+        </nav>
+        
+<!--SLIDE DE FOTOS -->
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img//slide1.jpg" class="d-block w-100 imgSlide" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img//slide2.jpg" class="d-block w-100 imgSlide" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img//slide3.jpg" class="d-block w-100 imgSlide" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="bootstrap-4.1.3-dist/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="bootstrap-4.1.3-dist/js/popper.min.js"></script>
+    <script src="bootstrap-4.1.3-dist/js/bootstrap.js"></script>
+  </body>
+</html>
